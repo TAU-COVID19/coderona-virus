37d70d7dd5ed5f20d051ad5e2e66e56a3f3d8607
IBentu <itai.bentolila@gmail.com>
2021-03-20 14:08:05 +0200

    added test to run paper8 scenario

---
+++ test/test_paper_8.py
@@ -0,0 +1,20 @@
+from src.scenarios import paper_8
+from src.simulation.initial_infection_params import NaiveInitialInfectionParams
+from src.run_utils import RepeatJob, SimpleJob, run
+
+
+def test_paper_8_bug():
+    jobs = []
+    for percent_immune in [0.0, 0.5]:
+        for city in ["Holon", "Bene Beraq"]:
+            for initial_num_infected in [25, 100, 250, 500]:
+                name = "{}:{} initial infected:{} percent immune, paper_8_bug".format(city, initial_num_infected, percent_immune)
+                jobs.append(RepeatJob(SimpleJob(
+                                days=180,
+                                scenario_name = name,
+                                city_name=city,
+                                infection_params=NaiveInitialInfectionParams(initial_num_infected, per_to_Immune=percent_immune),
+                                interventions=paper_8()
+    ), 20))
+    run(jobs, with_population_caching=False, verbosity=False)
+
