4682ab8e748d588d60769acb2d24ce792f0995e2
IBentu <itai.bentolila@gmail.com>
2021-03-20 17:04:05 +0200

    fixed error in integration

--- test/test_readExcel.py
+++ test/test_readExcel.py
@@ -1,22 +1,15 @@
-import json
-from logging import exception
 import math
 import multiprocessing as mp
-import os
-import pytest
-import sys 
+from concurrent.futures import ProcessPoolExecutor
 
-from src.logs import Statistics
-from src.simulation.params import Params
-from src.simulation.initial_infection_params import NaiveInitialInfectionParams
-from src.run_utils import generate_all_cities_for_jobs,SimpleJob, run
+from src.run_utils import generate_all_cities_for_jobs, SimpleJob
 from src.world.population_generation import PopulationLoader
 from src.world.city_data import get_city_list_from_dem_xls
 
 
-def test_Init_haifa(cities_path):
+def test_Init_haifa(params, cities_path):
     population_loader = PopulationLoader(
-            citiesDataPath,
+            cities_path,
             added_description="",
             with_caching=False,
             verbosity=False
@@ -26,26 +19,26 @@ def test_Init_haifa(cities_path):
 
 
 def test_Init_haifaParms(cities_path):
-    pop = population_loader.PopulationLoader(cities_path)
+    pop = PopulationLoader(cities_path)
     City1 = pop.get_city_by_name('Haifa')
     assert City1.region == 3
     assert City1.nafa == 31
 
-def test_GetCities():
+def test_GetCities(cities_path):
     #There are only 198 cities that we know all the needed data
     lst  = get_city_list_from_dem_xls(cities_path)
     assert len(lst) == 198
 
 def test_generate_all_cities_for_jobs_parallel():
-    citiesDataPath = ""
     params_to_change = {
         ("disease_parameters", "infectiousness_per_stage", "critical"): 1
     }
     test_jobs = [
         SimpleJob("test_default", 'kefar yona', 1.0,params_to_change=params_to_change),
         SimpleJob("test_default2", 'kefar yona', 1.0,params_to_change=params_to_change),
-        SimpleJob("test_default3", 'kefar yona', 1.0,params_to_change=params_to_change)]  
-    generate_all_cities_for_jobs(jobs=test_jobs,cpus_to_use=int(math.floor(mp.cpu_count()*0.9)))
+        SimpleJob("test_default3", 'kefar yona', 1.0,params_to_change=params_to_change)]
+    with ProcessPoolExecutor(int(math.floor(mp.cpu_count()*0.9))) as executor:
+        generate_all_cities_for_jobs(jobs=test_jobs, process_executor=executor)
     
 def test_generate_all_cities_for_jobs_serial(params, cities_path):
 
