ff4221a2976eec5e962ea13804829584a06f7373
IBentu <itai.bentolila@gmail.com>
2021-04-01 18:48:19 +0300

    removed a loop layer in a test

--- test/test_paper_8.py
+++ test/test_paper_8.py
@@ -1,3 +1,7 @@
+import os
+import sys
+sys.path.append(os.path.join(os.path.dirname(__file__), '..')) # Adding the src folder to PYTHONPATH
+
 from src.scenarios import paper_8
 from src.simulation.initial_infection_params import NaiveInitialInfectionParams
 from src.run_utils import RepeatJob, SimpleJob, run
@@ -5,16 +9,17 @@ from src.run_utils import RepeatJob, SimpleJob, run
 
 def test_paper_8_bug():
     jobs = []
-    for percent_immune in [0.0, 0.5]:
-        for city in ["Holon", "Bene Beraq"]:
-            for initial_num_infected in [25, 100, 250, 500]:
-                name = "{}:{} initial infected:{} percent immune, paper_8_bug".format(city, initial_num_infected, percent_immune)
-                jobs.append(RepeatJob(SimpleJob(
-                                days=180,
-                                scenario_name = name,
-                                city_name=city,
-                                infection_params=NaiveInitialInfectionParams(initial_num_infected, per_to_Immune=percent_immune),
-                                interventions=paper_8()
+    for city in ["Holon", "Bene Beraq"]:
+        for initial_num_infected in [25, 100, 250, 500]:
+            name = "paper 8 bug_City:{}_Initial infected:{}".format(city, initial_num_infected)
+            jobs.append(RepeatJob(SimpleJob(
+                            days=180,
+                            scenario_name = name,
+                            city_name=city,
+                            infection_params=NaiveInitialInfectionParams(initial_num_infected),
+                            interventions=paper_8()
     ), 20))
     run(jobs, with_population_caching=False, verbosity=False)
 
+if __name__ == "__main__":
+    test_paper_8_bug()
