4a23acacffd756da5c1ddf5f906b98e018563f4c
IBentu <itai.bentolila@gmail.com>
2021-04-07 15:19:38 +0300

    updating file name

--- my_run.sh
+++ my_run.sh
@@ -5,5 +5,5 @@ sudo docker rmi coderona_image
 sudo docker build -t coderona_image .
 sudo docker run --name coderona_container coderona_image &
 sleep 3
-sudo docker stats --format "table {{.Name}}\t{{.MemUsage}}" coderona_container > Bene_beraq_info_singleCpu_paper1.txt
+sudo docker stats --format "table {{.Name}}\t{{.MemUsage}}" coderona_container > Bene_beraq_info_CPU2_paper1.txt
 
