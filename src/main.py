dcc7af72c776d9cad4cdfd02de03d794217aadbc
Noam-BZ <77048119+Noam-BZ@users.noreply.github.com>
2021-05-06 10:35:15 +0300

    change to 100 rep and reduce base-infect. rep

--- src/main.py
+++ src/main.py
@@ -160,9 +160,9 @@ def main():
                                     # add job to make r to base infectiousness graph:
                                     jobs += [make_base_infectiousness_to_r_job(
                                                 'r_graph_' + full_scenario_name, city_name, scale,
-                                                np.arange(0.05, 0.15, 0.05),  # [0.03, 0.06, 0.1, 0.13, 0.16, 0.2],
+                                                np.arange(0.04, 0.1, 0.03),  # [0.03, 0.06, 0.1, 0.13, 0.16, 0.2],
                                                 interventions=intervention_scheme(compliance, ci_delay, hi_delay),
-                                                num_repetitions=20, days=20)]
+                                                num_repetitions=40, days=20)]
     # this start the run of the jobs
     run(jobs, multi_processed=True, with_population_caching=False, verbosity=False)
 
