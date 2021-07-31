4abd6e37b8c7c6001968fec749b3a65612ce96a0
Noam-BZ <77048119+Noam-BZ@users.noreply.github.com>
2021-07-31 22:01:54 +0300

    by general- older

--- src/extensions/ImmuneByAgeExtension.py
+++ src/extensions/ImmuneByAgeExtension.py
@@ -30,11 +30,11 @@ class ImmuneByAgeExtension(Simulation):
     def __init__(self, parent: Simulation):
         # change the following parameters to affect the vaccination flow
         self.target_immune_percentage = 0.5
-        self.min_age_to_immune = 0
+        self.min_age_to_immune = 18
         self.max_age_to_immune = 100
         self.max_people_to_immune_a_day = 800
-        self.immune_by_households = True # set to False, will Not immune all family members first
-        self.immune_strategy: ImmuneStrategy = ImmuneStrategy(ImmuneStrategy.ANY_AGE)
+        self.immune_by_households = False # set to False, will Not immune all family members first
+        self.immune_strategy: ImmuneStrategy = ImmuneStrategy(ImmuneStrategy.OLDER_TO_YOUNGER)
 
         # internal state. do not change!
         self.parent = parent
