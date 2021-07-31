c66eccce9f2b7c2c3b21ebfb2920856b6ab93137
Noam-BZ <77048119+Noam-BZ@users.noreply.github.com>
2021-07-31 22:16:33 +0300

    by general- younger

--- src/extensions/ImmuneByAgeExtension.py
+++ src/extensions/ImmuneByAgeExtension.py
@@ -34,7 +34,7 @@ class ImmuneByAgeExtension(Simulation):
         self.max_age_to_immune = 100
         self.max_people_to_immune_a_day = 800
         self.immune_by_households = False # set to False, will Not immune all family members first
-        self.immune_strategy: ImmuneStrategy = ImmuneStrategy(ImmuneStrategy.OLDER_TO_YOUNGER)
+        self.immune_strategy: ImmuneStrategy = ImmuneStrategy(ImmuneStrategy.YOUNGER_TO_OLDER)
 
         # internal state. do not change!
         self.parent = parent
