f1b5eb70c1e03664389b5eb0dd86428fcbd7633e
Noam-BZ <77048119+Noam-BZ@users.noreply.github.com>
2021-08-11 12:57:03 +0300

    vaccinate whole house,  ANY AGE  by general; 25 and 100 initial infected

--- src/extensions/ImmuneByAgeExtension.py
+++ src/extensions/ImmuneByAgeExtension.py
@@ -52,8 +52,8 @@ class ImmuneByAgeExtension(Simulation):
         self.max_age_to_immune = 100
         self.max_people_to_immune_a_day = 800
         self.immune_strategy: ImmuneStrategy = ImmuneStrategy(
-            order=ImmuneStrategy.YOUNGER_TO_OLDER,
-            immune_by_households=True,
+            order=ImmuneStrategy.ANY_AGE,
+            immune_by_households=False,
             immune_everybody_in_the_house=True)
 
         # internal state. do not change!
