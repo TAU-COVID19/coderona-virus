56733b9ef54e8aaa1483ce56ac7fe5eab2ddc069
Noam-BZ <77048119+Noam-BZ@users.noreply.github.com>
2021-08-11 20:43:54 +0300

    vaccinate whole house,  Older To Younger by general; 80% vaccinated

--- src/extensions/ImmuneByAgeExtension.py
+++ src/extensions/ImmuneByAgeExtension.py
@@ -52,8 +52,8 @@ class ImmuneByAgeExtension(Simulation):
         self.max_age_to_immune = 100
         self.max_people_to_immune_a_day = 800
         self.immune_strategy: ImmuneStrategy = ImmuneStrategy(
-            order=ImmuneStrategy.ANY_AGE,
-            immune_by_households=True,
+            order=ImmuneStrategy.OLDER_TO_YOUNGER,
+            immune_by_households=False,
             immune_everybody_in_the_house=False)
 
         # internal state. do not change!
