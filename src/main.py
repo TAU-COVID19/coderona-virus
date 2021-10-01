e58a3768a452722743e7e0fe95e52e8447cb4247
Noam-BZ <77048119+Noam-BZ@users.noreply.github.com>
2021-10-01 10:22:43 +0300

    NEIGHBORHOOD STRATEGY BY WEEK-MEAN. TEST TO RUN; FIXED COMPLIANCE FOR SCENARIOS. compliance=0.5

--- src/main.py
+++ src/main.py
@@ -165,7 +165,7 @@ def main():
             for immune_source, min_age in [(InitialImmuneType.GENERAL_POPULATION, 18),(InitialImmuneType.HOUSEHOLDS, 18), (InitialImmuneType.BY_NEIGHBORHOOD, 18), (InitialImmuneType.HOUSEHOLDS_ALL_AT_ONCE,18)]:  # the options are:GENERAL_POPULATION,HOUSEHOLDS
                 for initial_num_infected in [100, 500]:  # [25, 100, 250, 500]:
                     for city_name, scale in [("Bene Beraq", 1), ("Holon", 1)]:  # [("Bene Beraq", 1), ("Holon", 1)]
-                        for compliance in [0.7]:
+                        for compliance in [0.5]:
                             for order in [ORDER.DESCENDING, ORDER.ASCENDING]:
                                 for ci_delay in [4]:
                                     for hi_delay in [4]:
