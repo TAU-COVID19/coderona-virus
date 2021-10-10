b577b90993b9bfe94b7ebfcb05f62c81bf55f95a
noambenzuk@mail.tau.ac.il <Ziv.BenZuk@agoda.com>
2021-10-10 11:39:40 +0800

    add utility to zip the CSV files required to create later the report using src/create_infection_summary.py

---
+++ zip_statistics.sh
@@ -0,0 +1,19 @@
+
+echo "------ Zipping Statistics... ------"
+if [ $# -eq 0 ] ; then
+    echo "ERROR! please provide one argument which is the name of the directory holding the run output"
+    exit 1
+fi
+
+if [ ! -d "outputs/$1" ]; then
+  echo "ERROR! directory" + "outputs/"$1 + " DO NOT EXIST!!"
+  exit 2
+fi
+
+pushd `pwd`
+cd "outputs"
+pwd
+zip -r ../statistics_csv_"$1".zip "$1" -i \*daily_delta.csv \*amit_graph_integral.csv \*amit_graph_daily.csv \*amit_graph\*.csv
+popd
+pwd
+echo "------ Zipping Statistics - COMPLETE (statistics_csv.zip) ------"
