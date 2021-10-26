f84279dbe41e553b5e93daaf377825f25196a2d4
Noam-BZ <77048119+Noam-BZ@users.noreply.github.com>
2021-10-26 13:44:24 +0300

    test no. Initial infected= 500+empty scenario

--- src/main.py
+++ src/main.py
@@ -128,7 +128,7 @@ def main():
         # "paper_2_comp_9": paper_2_comp_9,
         # "vaccinations_scenario_general": vaccinations_scenario_general,
         # "vaccinations_scenario_households": vaccinations_scenario_households,
-        #"Empty_scenario": Empty_scenario,
+        "Empty_scenario": Empty_scenario,
         "school_closure": children_school_closure_intervention,
         "asymptomatic_detection": children_asymptomatic_detection_intervention,
         #"only_children_asymptomatic_detection":only_children_asymptomatic_detection
@@ -163,7 +163,7 @@ def main():
     for target_immune_percentage, immune_compliance in [(0.8, 1.0)]:  # [(0.0,1),(0.5,1)]:
         for people_per_day in [800]:
             for immune_source, min_age in [(InitialImmuneType.GENERAL_POPULATION, 18), (InitialImmuneType.HOUSEHOLDS, 18), (InitialImmuneType.BY_NEIGHBORHOOD, 18), (InitialImmuneType.HOUSEHOLDS_ALL_AT_ONCE,18)]:  # the options are:GENERAL_POPULATION,HOUSEHOLDS
-                for initial_num_infected in [100]:  # [25, 100, 250, 500]:
+                for initial_num_infected in [500]:  # [25, 100, 250, 500]:
                     for city_name, scale in [("Bene Beraq", 1), ("Holon", 1)]:  # [("Bene Beraq", 1), ("Holon", 1)]
                         for compliance in [0.7]:
                             for order in [ORDER.DESCENDING, ORDER.ASCENDING]:
