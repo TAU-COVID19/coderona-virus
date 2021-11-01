7b9cb032caf68c6d5d30eef3fe7b2b3457d4f731
Noam-BZ <77048119+Noam-BZ@users.noreply.github.com>
2021-11-01 18:38:17 +0200

    test more initial infected

--- src/main.py
+++ src/main.py
@@ -106,7 +106,7 @@ def main():
         # "scenario_365": scenario_365_interventions,
         # "scenario_395": scenario_395_interventions,
         # "reality1" : scenario_reality1,
-        "hh_isolation": householdisolation_sd_interventions,
+        #"hh_isolation": householdisolation_sd_interventions,
         # "check" : scenario_check,
         # "reality2" : scenario_reality2,
         # "reality3": scenario_reality3,
@@ -129,7 +129,7 @@ def main():
         # "vaccinations_scenario_general": vaccinations_scenario_general,
         # "vaccinations_scenario_households": vaccinations_scenario_households,
         #Empty_scenario": Empty_scenario,
-        "school_closure": children_school_closure_intervention,
+        #"school_closure": children_school_closure_intervention,
         "asymptomatic_detection": children_asymptomatic_detection_intervention,
         #"only_children_asymptomatic_detection":only_children_asymptomatic_detection
         #"noHH_children_specific_interventions": children_specific_noHH_interventions,
@@ -163,9 +163,9 @@ def main():
     for target_immune_percentage, immune_compliance in [(0.8, 1.0)]:  # [(0.0,1),(0.5,1)]:
         for people_per_day in [800]:
             for immune_source, min_age in [(InitialImmuneType.GENERAL_POPULATION, 18), (InitialImmuneType.HOUSEHOLDS, 18), (InitialImmuneType.BY_NEIGHBORHOOD, 18)]:  # the options are:GENERAL_POPULATION,HOUSEHOLDS
-                for initial_num_infected in [100]:  # [25, 100, 250, 500]:
+                for initial_num_infected in [100, 500]:  # [25, 100, 250, 500]:
                     for city_name, scale in [("Bene Beraq", 1), ("Holon", 1)]:  # [("Bene Beraq", 1), ("Holon", 1)]
-                        for compliance in [0.7]:
+                        for compliance in [0.7, 0.5]:
                             for order in [ORDER.DESCENDING, ORDER.ASCENDING]:
                                 for ci_delay in [4]:
                                     for hi_delay in [4]:
