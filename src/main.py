7a2f2244db609f5859b662ef6a37eb8f0be85027
Noam-BZ <77048119+Noam-BZ@users.noreply.github.com>
2021-11-20 08:16:44 +0200

    test a longer run for 150 days and two ammounts of vaccines

--- src/main.py
+++ src/main.py
@@ -161,7 +161,7 @@ def main():
     jobs = []
 
     for target_immune_percentage, immune_compliance in [(0.8, 1.0)]:  # [(0.0,1),(0.5,1)]:
-        for people_per_day in [800]:
+        for people_per_day in [500, 800]:
             for immune_source, min_age in [(InitialImmuneType.GENERAL_POPULATION, 18), (InitialImmuneType.HOUSEHOLDS, 18), (InitialImmuneType.BY_NEIGHBORHOOD, 18)]:  # the options are:GENERAL_POPULATION,HOUSEHOLDS
                 for initial_num_infected in [100]:  # [25, 100, 250, 500]:
                     for city_name, scale in [("Bene Beraq", 1), ("Holon", 1)]:  # [("Bene Beraq", 1), ("Holon", 1)]
@@ -196,7 +196,7 @@ def main():
                                                                                                 minimum_infectiousness_age)
                                                     #                                    full_scenario_name = "res"
                                                     jobs.append(RepeatJob(SimpleJob(full_scenario_name,
-                                                                                    days=120,
+                                                                                    days=150,
                                                                                     city_name=city_name,
                                                                                     scale=scale,
                                                                                     infection_params=NaiveInitialInfectionParams(
