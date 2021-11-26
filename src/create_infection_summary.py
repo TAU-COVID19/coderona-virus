d2af65d7761799ea76b64874396d21a11d6b8e25
noambenzuk@mail.tau.ac.il <Ziv.BenZuk@agoda.com>
2021-11-26 15:29:40 +0800

    add daily infection & R graphs

--- src/create_infection_summary.py
+++ src/create_infection_summary.py
@@ -1,17 +1,30 @@
 import math
 import os
+import json
+from enum import Enum
 import statistics
 import sys
 from collections import namedtuple
 from typing import List
+import numpy as np
+from w import W
+from src.simulation.params import Params
 
 import pandas
 from functional import seq, pipeline
 from matplotlib import pyplot
 
+
+class GraphType(Enum):
+    BAR = 1
+    BOX = 2
+    VIOLIN = 3
+
+
 # can draw either bars or boxplot
-draw_bar = True
+selected_graph_type: GraphType = GraphType.VIOLIN
 draw_points_on_graph = False
+w = None
 
 
 class Categories:
@@ -130,15 +143,6 @@ def get_daily_info(root_path) -> DAILY_INFO:
     daily_infection = None
 
     for i in range(1000):
-        # infected_today = get_sample_line(root_path, i, "integral", 1)
-        # critical_today = get_daily_column(root_path, sample=i, column=9)
-        # if infected_today is not None and critical_today is not None:
-        #     infected_sum.append(sum(critical_today))
-        #     max_infectious_in_community.append(max(infected_today))
-        #     number_of_samples += 1
-        # else:
-        #     break
-
         infected_today = get_daily_column(root_path, sample=i, column_name="infected_today")
         total_infected_in_community = get_sample_line(root_path, i, "integral", line_name="infected_0_99")
 
@@ -171,9 +175,9 @@ def get_daily_info(root_path) -> DAILY_INFO:
             break
 
         if daily_critical_cases is None:
-            daily_critical_cases = seq(critical_today)
+            daily_critical_cases = seq(total_critical_in_community)
         else:
-            daily_critical_cases = daily_critical_cases.zip(seq(critical_today)).map(lambda x: x[0] + x[1])
+            daily_critical_cases = daily_critical_cases.zip(seq(total_critical_in_community)).map(lambda x: x[0] + x[1])
 
     daily_critical_cases = daily_critical_cases.map(lambda x: x / number_of_samples)
 
@@ -241,7 +245,58 @@ def draw_daily_graphs(df, ax, plot_infection_graph):
             ax.legend()
 
 
+def draw_daily_r_graph(df, ax):
+    print(f"draw_daily_r_graph() start...")
+    for key, daily_results in enumerate(df["daily_infection"]):
+        r = calculate_r0_instantaneous(daily_results)
+        color, line_style, label = select_daily_graph_colors(df["vaccination_strategy"].to_list()[key],
+                                                             df["order"].to_list()[key])
+        ax.plot(range(len(r)), r, label=label,
+                color=color,
+                linestyle=line_style)
+        ax.legend()
+    print(f"draw_daily_r_graph() complete.")
+
+
+def calculate_r0_instantaneous(infected_per_day: List[int], look_back_days: int = 7) -> List[float]:
+    """calculates the instantaneous r based on a look back of look_back_days days"""
+
+    r = []
+
+    for i in range(len(infected_per_day)):
+        if i <= look_back_days:
+            r.append(0)
+        else:
+            # https://www.ncbi.nlm.nih.gov/pmc/articles/PMC7325187/
+            look_back_days = w.w_len()
+
+            r_today = infected_per_day[i] \
+                      / max(seq([w.get_w(i) *
+                                 infected_per_day[i - look_back]
+                                 for look_back in range(1, look_back_days)]).sum(), 1)
+            r.append(r_today)
+
+    return r
+
+
+def set_axis_style(ax, labels):
+    ax.xaxis.set_tick_params(direction='out')
+    ax.xaxis.set_ticks_position('bottom')
+    # tick_label = ax.set_ticklabels(labels)
+    ax.xaxis.set_ticks(ticks=np.arange(1, len(labels) + 1))
+    ax.xaxis.set_ticklabels(labels)
+    ax.set_xlim(0.25, len(labels) + 0.75)
+    # ax.set_xlabel('Sample name')
+
+
 if __name__ == "__main__":
+    config_path = os.path.join(os.path.dirname(__file__), "config.json")
+    with open(config_path) as json_data_file:
+        ConfigData = json.load(json_data_file)
+        paramsDataPath = ConfigData['ParamsFilePath']
+    Params.load_from(os.path.join(os.path.dirname(__file__), paramsDataPath), override=True)
+    w = W()
+
     if len(sys.argv) != 2:
         print("ERROR! please provide one argument which is the date/time of the run")
         exit(-1)
@@ -305,7 +360,7 @@ if __name__ == "__main__":
 
     fig, axs = pyplot.subplots(len(categories) * 4, 1)
     fig.set_figwidth(16)
-    fig.set_figheight(len(categories) * 20)
+    fig.set_figheight(len(categories) * 40)
 
     fig2, axs2 = pyplot.subplots(len(categories) * 4, 1)
     fig2.set_figwidth(16)
@@ -327,58 +382,78 @@ if __name__ == "__main__":
         axs[category_i].plot([-1, 1.5], [1.3, 1.3], color='palevioletred', lw=3, transform=axs[category_i].transAxes,
                              clip_on=False)
 
-        if draw_bar:
+        if selected_graph_type == GraphType.BAR:
             axs[category_i].bar(df["immune_order"], df["total_infected"], color="lightsteelblue")
             axs[category_i].errorbar(df["immune_order"], df["total_infected"], yerr=df["std_infected"], capsize=10,
                                      ecolor="cornflowerblue", fmt=".")
             if draw_points_on_graph:
                 axs[category_i].plot(df["immune_order"], df["infected_sum"].to_list(), "o")
-        else:
+        elif selected_graph_type == GraphType.BOX:
             axs[category_i].boxplot(df["infected_sum"], labels=df["immune_order"])
+        else:
+            axs[category_i].violinplot(df["infected_sum"], showmedians=True, showextrema=False)
+            set_axis_style(axs[category_i], df["immune_order"])
 
         draw_daily_graphs(df, axs2[daily_category_i], plot_infection_graph=True)
         axs2[daily_category_i].set_title(f"Daily Infected ({title})")
         daily_category_i += 1
 
         draw_daily_graphs(df, axs2[daily_category_i], plot_infection_graph=False)
-        axs2[daily_category_i].set_title(f"Daily Critical Cases ({title})")
+        axs2[daily_category_i].set_title(f"Total Critical Cases ({title})")
         daily_category_i += 1
 
-        axs[category_i].set_title(f"Total Infected ({title})")
+        draw_daily_r_graph(df, axs2[daily_category_i])
+        axs2[daily_category_i].set_title(f"Daily Instantaneous R ({title})")
+        daily_category_i += 1
+
+        axs[category_i].set_title(f"Daily Infected ({title})")
         category_i += 1
 
-        if draw_bar:
+        if selected_graph_type == GraphType.BAR:
             axs[category_i].bar(df["immune_order"], df["total_critical"], color="thistle")
             axs[category_i].errorbar(df["immune_order"], df["total_critical"], yerr=df["std_critical"], capsize=10,
                                      ecolor="slateblue", fmt=".")
             if draw_points_on_graph:
                 axs[category_i].plot(df["immune_order"], df["critical_sum"].to_list(), "o")
-        else:
+        elif selected_graph_type == GraphType.BOX:
             axs[category_i].boxplot(df["critical_sum"], labels=df["immune_order"])
+        else:
+            parts = axs[category_i].violinplot(df["critical_sum"], showmedians=True, showextrema=False)
+            set_axis_style(axs[category_i], df["immune_order"])
+            for pc in parts['bodies']:
+                pc.set_facecolor('#D43F3A')
+                pc.set_edgecolor('black')
+                pc.set_alpha(1)
 
         axs[category_i].set_title(f"Total Critical ({title})")
         category_i += 1
 
-        if draw_bar:
+        if selected_graph_type == GraphType.BAR:
             axs[category_i].bar(df["immune_order"], df["max_infected"], color="lightsteelblue")
             axs[category_i].errorbar(df["immune_order"], df["max_infected"], yerr=df["std_max_infected"], capsize=10,
                                      ecolor="cornflowerblue", fmt=".")
             if draw_points_on_graph:
                 axs[category_i].plot(df["immune_order"], df["infected_max"].to_list(), "o")
-        else:
+        elif selected_graph_type == GraphType.BOX:
             axs[category_i].boxplot(df["infected_max"], labels=df["immune_order"])
+        else:
+            axs[category_i].violinplot(df["infected_max"], showmedians=True, showextrema=False)
+            set_axis_style(axs[category_i], df["immune_order"])
 
         axs[category_i].set_title(f"Max Infected ({title})")
         category_i += 1
 
-        if draw_bar:
+        if selected_graph_type == GraphType.BAR:
             axs[category_i].bar(df["immune_order"], df["max_critical"], color="thistle")
             axs[category_i].errorbar(df["immune_order"], df["max_critical"], yerr=df["std_max_critical"], capsize=10,
                                      ecolor="slateblue", fmt=".")
             if draw_points_on_graph:
                 axs[category_i].plot(df["immune_order"], df["critical_max"].to_list(), "o")
-        else:
+        elif selected_graph_type == GraphType.BOX:
             axs[category_i].boxplot(df["critical_max"], labels=df["immune_order"])
+        else:
+            axs[category_i].violinplot(df["critical_max"], showmedians=True, showextrema=False)
+            set_axis_style(axs[category_i], df["immune_order"])
 
         axs[category_i].set_title(f"Max Critical ({title})")
 
