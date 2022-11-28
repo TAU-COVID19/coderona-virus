4aebf7bc714bd60b13c1e70734fa2da4399bec78
Noam-BZ <77048119+Noam-BZ@users.noreply.github.com>
2022-11-28 17:29:00 +0200

    run simulation to infer using ABC model no intervention, no vaccination, changing asymptomatic infectiousness to be as symptomatic infectiousness 100 initial infected

--- src/main.py
+++ src/main.py
@@ -164,7 +164,7 @@ def main():
     for target_immune_percentage, immune_compliance in [(0.8, 1)]:  # [(0.0,1),(0.5,1)]:
         for people_per_day in [0]:
             for immune_source, min_age in [(InitialImmuneType.BY_NEIGHBORHOOD, 18)]:  # the options are:GENERAL_POPULATION,HOUSEHOLDS
-                for initial_num_infected in [1]:  # [25, 100, 250, 500]:
+                for initial_num_infected in [100]:  # [25, 100, 250, 500]:
                     for city_name, scale in [("Holon", 1)]:  # [("Bene Beraq", 1), ("Holon", 1)]
                         for compliance in [0.7]:
                             for order in [ORDER.ASCENDING]:
